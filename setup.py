87b74f89aada8311bcb40600df1ce7f1ecaec096
Ondrej Platek <ondrej.platek@seznam.cz>
2015-01-14 21:37:12 +0100

    updated dependencies versions

--- setup.py
+++ setup.py
@@ -102,6 +102,6 @@ setup(
     packages=['fst'],
     ext_modules=ext_modules,
     test_suite='nose.collector',
-    setup_requires=['cython>=0.19.1', 'pyyaml', 'pystache'],
-    tests_require=['nose>=1.0', 'pyfst'],
+    install_requires=['cython>=0.21', 'pystache>=0.5', 'pyyaml>=3.11'],
+    tests_require=['nose>=1.0'],
 )
